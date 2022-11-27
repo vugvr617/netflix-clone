664e3f87712ee7653a7e53c8b6c7df22496a53ca
Vugar <nasraddinli@digitale-lernumgebung.de>
2022-11-27 03:15:15 +0100

    functionality added to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { SearchIcon, BellIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkScroll = (): void => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", checkScroll);
+
+    return () => {
+        removeEventListener('scroll', checkScroll);
+    }
+  }, []);
+
   return (
-    <header>
-      <div className="flex flex-row items-center space-x-2 md:space-x-10 px-4">
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex flex-row items-center space-x-2 md:space-x-10">
         <img
           src="https://cdn-icons-png.flaticon.com/512/5977/5977590.png"
           width={130}
@@ -14,6 +36,18 @@ const Header = () => {
           <li className="header-link">New & Popular</li>
           <li className="header-link">My List</li>
         </ul>
+      </div>
+      <div className="flex items-center space-x-4 font-light">
+        <SearchIcon className="hidden text-[#e5e5e5] h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="text-[#e5e5e5] h-6 w-6" />
+        <Link href="./account">
+          <img
+            src="https://i.pinimg.com/736x/db/70/dc/db70dc468af8c93749d1f587d74dcb08.jpg"
+            alt="profile-picture"
+            className="cursor-pointer h-6 w-6 rounded"
+          ></img>
+        </Link>
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative h-screen">
       <Head>
         <title>Home - Netflix</title>
       </Head>
